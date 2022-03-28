99727cdd736b3583226b1c1ed1b22bb42f0b0073
Dasari Bhovan <dasaribhovan@gmail.com>
2022-03-28 21:19:57 +0530

    Update settings.py

--- bidding_system/settings.py
+++ bidding_system/settings.py
@@ -1,5 +1,6 @@
 import django_heroku
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,7 +74,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+dj_from_env=dj_database_url.config(conn_max_age=200)
+DATABASES['default'].update(dj_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
